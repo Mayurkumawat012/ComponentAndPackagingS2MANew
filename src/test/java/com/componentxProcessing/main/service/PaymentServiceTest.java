5c7983848df20488384f45e16c230787c1bcee71
mayurkumawat012 <mayurkumawat012@gmail.com>
2021-05-31 13:59:35 +0530

    change in test service

--- src/test/java/com/componentxProcessing/main/service/PaymentServiceTest.java
+++ src/test/java/com/componentxProcessing/main/service/PaymentServiceTest.java
@@ -1,14 +1,37 @@
 package com.componentxProcessing.main.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
-class PaymentServiceTest {
+import com.componentxProcessing.main.client.PaymentClient;
+import com.componentxProcessing.main.dto.PackagingAndDeliveryDTO;
 
+@SpringBootTest
+public class PaymentServiceTest {
+   
+	@InjectMocks
+	PaymentService paymentService;
+	
+	@Mock
+	PaymentClient paymentClient;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Checking for ProcessRequestService - if it is loading or not for @GetMapping")
+	 void repairServiceImplNotNullTest() {
+		assertThat(paymentService).isNull();
 	}
-
+	
+	@Test
+	public void testMessageConfirmation() {
+		when(paymentClient.paymentDetails("requestId", "creditCardNumber", 100000, 500, "token")).thenReturn(new PackagingAndDeliveryDTO(0.0));
+		String message = paymentService.messageConfirmation("requestId", "creditCardNumber", 100000, 500, "token");
+		assertEquals("Operation Not Successful", message);
+	}
 }
